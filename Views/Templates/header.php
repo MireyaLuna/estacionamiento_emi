9ca3a6c5034bc724de6409e741f34715a5541a51
Mireya Luna <adrianaluna737@gmail.com>
2022-11-24 21:17:42 -0400

    :art: Logo EMI

--- Views/Templates/header.php
+++ Views/Templates/header.php
@@ -14,7 +14,8 @@
     <body class="sb-nav-fixed">
         <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
             <!-- Navbar Brand-->
-            <a class="navbar-brand ps-3" href="index.html">EMI</a>
+            <a class="navbar-brand ps-3" href="index.html"><img
+                src="<?php echo base_url;?>Assets/img/logo_emi.png" height="56" width="200"></a>
             <!-- Sidebar Toggle-->
             <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i class="fas fa-bars"></i></button>
             <!-- Navbar Search-->
